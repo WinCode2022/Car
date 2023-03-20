519f1fd271c0d09b481b00b20c46cbd62d70fde5
Win Code <109026541+WinCode2022@users.noreply.github.com>
2023-03-20 15:19:23 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const slides=document.querySelectorAll(".slide")
+
+for(const slide of slides){
+    slide.addEventListener("click",()=>{
+        clearActiveClases()
+slide.classList.add("active")
+    })
+
+}
+function clearActiveClases(){
+    slides.forEach((slide)=>{
+slide.classList.remove("active")
+    })
+}
